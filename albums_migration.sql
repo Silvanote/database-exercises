4fbec4f966c3e884e25343e527f576324d0dc9b3
Juan Silva <juan.silvawd@gmail.com>
2022-07-06 16:25:26 -0500

    exercises

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+    );
---
+++ basic_statements_lec.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.');
+

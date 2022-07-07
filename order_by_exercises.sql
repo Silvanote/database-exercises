978c62347a3d40accec62577eed1810932c7c37e
Juan Silva <juan.silvawd@gmail.com>
2022-07-07 16:41:30 -0500

    updates

---
+++ order_by_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+-- SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name  DESC;
+
+-- SELECT * FROM employees WHERE first_name = 'Irene' OR first_name= 'Vidya' OR first_name = 'Maya';
+# SELECT first_name, last_name
+# FROM employees
+# ORDER BY first_name ASC;
+
+-- SELECT * FROM employees WHERE first_name = 'Irena' OR first_name= 'Vidya' OR first_name = 'Maya'
+--     AND gender IN ('M');
+--
+-- SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+# --
+SELECT * FROM employees WHERE last_name LIKE ('%e%')
+ORDER BY emp_no DESC;
+--
+--
+-- SELECT * FROM employees WHERE last_name LIKE ('%q%');
+--
+--
+-- SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';

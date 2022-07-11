cc1e78e6052c89d29558026823f765957d979811
Juan Silva <juan.silvawd@gmail.com>
2022-07-11 16:34:58 -0500

    updated join exer

--- join_exercises.sql
+++ join_exercises.sql
@@ -27,7 +27,7 @@ SELECT t.title, COUNT(t.title) as Total
 FROM employees as e
          JOIN titles as t
               ON e.emp_no = t.emp_no
-         JOIN dept_manager as de
+         JOIN dept_emp as de
               ON e.emp_no = de.emp_no
          JOIN departments as d
               ON d.dept_no = de.dept_no
@@ -35,15 +35,28 @@ WHERE t.to_date = '9999-01-01' AND de.to_date = '9999-01-01' AND d.dept_name = '
 GROUP BY t.title;
 
 -- --4. QUERY SALARY OF ALL CURRENT MANAGERS
-SELECT t.title, COUNT(t.title) as Total
-FROM employees as e
-         JOIN titles as t
-              ON e.emp_no = t.emp_no
-         JOIN dept_manager as de
-              ON e.emp_no = de.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE t.to_date = '9999-01-01' AND de.to_date = '9999-01-01' AND d.dept_name = 'Customer Service'
-GROUP BY t.title;
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name)
+    AS 'Department Manager', s.salary AS 'Salary'
+FROM departments AS d
+         JOIN dept_manager AS dm
+             ON d.dept_no = dm.dept_no
+         JOIN employees AS e
+             ON dm.emp_no = e.emp_no
+         JOIN salaries AS s
+             ON e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND s.to_date = '9999-01-01'
+ORDER BY d.dept_name;
 
-# --BONUS
+
+# --BONUS --instructor's solution
+
+# SELECT CONCAT(e.first_name, ' ', e.last_name), d.dept_name, CONCAT(e2.first_name, ' ', e2.last_name)
+# FROM employees AS e
+#          JOIN dept_emp AS de ON e.emp_no = de.emp_no
+#          JOIN departments AS d ON de.dept_no = d.dept_no
+#          JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+#          JOIN employees AS e2 ON dm.emp_no = e2.emp_no
+# WHERE YEAR(de.to_date) = 9999 AND YEAR(dm.to_date) = 9999
+# ORDER BY d.dept_name, e.emp_no;
+#

ab6c06a55cd28ff441a0e4dbbb93b0c3c694b18d
Juan Silva <juan.silvawd@gmail.com>
2022-07-12 09:18:33 -0500

    only one wrong

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,41 +8,45 @@ USE employees;
 #               ON d.dept_no = de.dept_no
 # WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
+#Find all employees with same hire date as employee number 101010
 select * from employees as e
-where e.hire_date = (
-    select e.hire_date from employees as e where e.emp_no = 101010
+    where e.hire_date = (
+        select e.hire_date from employees as e where e.emp_no = 101010
 );
+# Find all the titles held by all employees name Aamod
 
-select distinct title from titles
-where emp_no IN (
-    select emp_no from employees where first_name = 'Aamod'
-);
-
-
-select first_name, last_name from employees
-where emp_no IN (
-    select emp_no from dept_manager
+select title from titles
     where emp_no IN (
+        select emp_no from employees where first_name = 'Aamod'
+);
+
+# FIND all the current dept managers that are female
+select first_name, last_name from employees
+    where emp_no IN (
+    select emp_no from dept_manager
+        where emp_no IN (
         select emp_no from employees where gender = 'F'
-    ) and to_date > now()
+    )
+          and to_date > CURDATE()
 ) ;
 
-
+#BONUS
 
 select dept_name from departments
-where dept_no IN (
-    select dept_no from dept_manager
-    where emp_no IN (
-        select emp_no from employees
-        where gender ='F'
-    ) and to_date > now()
+    where dept_no IN (
+        select dept_no from dept_manager
+        where emp_no IN (
+            select emp_no from employees
+            where gender ='F'
+    )
+          and to_date > CURDATE()
 );
 
 
 select first_name, last_name from employees
-where emp_no IN (
+    where emp_no IN (
     select emp_no from salaries
-    where salary = (
+        where salary = (
         select max(salary) from salaries
     )
 );
